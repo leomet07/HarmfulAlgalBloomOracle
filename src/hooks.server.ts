8873ffd520903b539eb0682910a006f8a1034bf2
leomet07 <leomet07@gmail.com>
2025-11-02 13:20:29 -0500

    do not connect to mongodb on server start

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,9 @@
 import { connect } from '$lib/db/mongo.server';
 
 // Connect to MongoDB before starting the server
-connect().then((): void => {
-    console.log("MongoDB started");
-}).catch((e: Error) => {
-    console.log("MongoDB failed to start");
-    console.log(e);
-});
+// connect().then((): void => {
+//     console.log("MongoDB started");
+// }).catch((e: Error) => {
+//     console.log("MongoDB failed to start");
+//     console.log(e);
+// });
