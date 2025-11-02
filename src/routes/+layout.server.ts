8bbd51afcaa07dc0ec319032d3117703c2aa7ba6
leomet07 <leomet07@gmail.com>
2025-11-02 13:05:49 -0500

    Temporarily make website load without data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,19 +2,19 @@ import { getLakes, getSpatialPredictionMaps } from "$lib/db/db_utils.server";
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ params }) => {
-    const [lakes, spatialPredictions] = await Promise.all([getLakes(), getSpatialPredictionMaps()]);
+    // const [lakes, spatialPredictions] = await Promise.all([getLakes(), getSpatialPredictionMaps()]);
 
-    let simpleRasterDates: Date[] = []
-    for (const spatialPredictionMap of spatialPredictions) {
-        simpleRasterDates.push(spatialPredictionMap.date);
-    }
+    // let simpleRasterDates: Date[] = []
+    // for (const spatialPredictionMap of spatialPredictions) {
+    //     simpleRasterDates.push(spatialPredictionMap.date);
+    // }
 
-    // make dates unique
-    simpleRasterDates = [...new Set(simpleRasterDates.map(r => r.getTime()))].map((r: number) => (new Date(r)));
-    simpleRasterDates.sort();
+    // // make dates unique
+    // simpleRasterDates = [...new Set(simpleRasterDates.map(r => r.getTime()))].map((r: number) => (new Date(r)));
+    // simpleRasterDates.sort();
     return {
-        lakes,
+        lakes: [],
         spatialPredictions: [],
-        simpleRasterDates
+        simpleRasterDates: []
     }
 };
