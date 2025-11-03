b15124f393e77956e55d2f3785ce60f69a16bea8
leomet07 <leomet07@gmail.com>
2025-11-02 23:31:53 -0500

    Changre default date to summer to show lake chautaqua defaults

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,4 +2,4 @@ import type { LatLngTuple } from 'leaflet';
 import { writable } from 'svelte/store';
 
 export const mapCoords = writable<LatLngTuple | undefined>();
-export const selectedDateYYYYMMDD = writable<string>("2022-09-14");
+export const selectedDateYYYYMMDD = writable<string>("2022-07-13");
